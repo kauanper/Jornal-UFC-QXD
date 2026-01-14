fdcc1baa177b68bc1206bf0111240af825d67c78
kauan <kauancavalcante8p@gmail.com>
2026-01-14 20:04:10 -0300

    add: criptografia em password

--- src/main/java/com/example/JornalUFC/testesExploratorios/UsersInitializer.java
+++ src/main/java/com/example/JornalUFC/testesExploratorios/UsersInitializer.java
@@ -8,25 +8,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UsersInitializer {
 
     @Bean
-    CommandLineRunner initUsers(UserRepository userRepository) {
+    CommandLineRunner initUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-            // Usuário ADMIN
             User admin = new User("admin@email.com", passwordEncoder.encode("admin123"), UserRoles.ADMIN);
             User editor = new User("editor@email.com", passwordEncoder.encode("editor123"), UserRoles.EDITOR);
-
             userRepository.save(admin);
             userRepository.save(editor);
-
-
             System.out.println("Usuários iniciais criados no H2!");
         };
     }
+
 }
