4cecd598c76f8aa10f7905eb085cd70e35cf1975
kauan <kauancavalcante8p@gmail.com>
2026-01-14 19:03:35 -0300

    fix: app stateless

--- src/main/java/com/example/JornalUFC/config/SecurityConfig.java
+++ src/main/java/com/example/JornalUFC/config/SecurityConfig.java
@@ -3,20 +3,26 @@ package com.example.JornalUFC.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())) //permite H2 em iframe
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // permite iframes para H2
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/h2-console/**").permitAll() // libera H2 console
+                        .anyRequest().permitAll() // resto também liberado
                 );
 
         return http.build();
     }
+
 }
