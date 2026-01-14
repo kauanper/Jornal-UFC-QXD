2fade574275c951868cc6596f5a9de9fa9723a05
kauan <kauancavalcante8p@gmail.com>
2026-01-14 15:38:51 -0300

    instanciando projeto

---
+++ src/main/java/com/example/JornalUFC/JornalUfcApplication.java
@@ -0,0 +1,13 @@
+package com.example.JornalUFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalUfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalUfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JornalUFC/JornalUfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JornalUFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornalUfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
