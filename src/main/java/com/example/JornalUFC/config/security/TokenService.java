79ad7e7625ed12606aa868e280a6817bf81683cf
kauan <kauancavalcante8p@gmail.com>
2026-01-14 23:58:19 -0300

    add: metodo generateToken

--- src/main/java/com/example/JornalUFC/config/security/TokenService.java
+++ src/main/java/com/example/JornalUFC/config/security/TokenService.java
@@ -1,9 +1,15 @@
 package com.example.JornalUFC.config.security;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.JornalUFC.modules.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -14,9 +20,20 @@ public class TokenService {
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256("secret");
-            return "token";
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            String tokenCreated = JWT.create()
+                    .withIssuer("jornal-ufc-qxd")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(getExpiration())
+                    .sign(algorithm);
+            return tokenCreated;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro na geração do token", e);
         }
+    }
+
+    private Instant getExpiration() {
+        return LocalDateTime.now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
     }
 }
