09126217246fa9dfe7ff7cacdd7a9936002cd9e6
kauan <kauancavalcante8p@gmail.com>
2026-01-17 04:20:20 -0300

    concluindo US10 — Visualizar usuários editores

--- src/main/java/com/example/JornalUFC/modules/user/UserController.java
+++ src/main/java/com/example/JornalUFC/modules/user/UserController.java
@@ -3,16 +3,16 @@ package com.example.JornalUFC.modules.user;
 import com.example.JornalUFC.modules.user.dtos.RegisterUserDTO;
 import com.example.JornalUFC.modules.user.dtos.ResponseUserDTO;
 import com.example.JornalUFC.modules.user.servicies.CreateUserUseCase;
+import com.example.JornalUFC.modules.user.servicies.GetAllEditorsUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -21,10 +21,20 @@ public class UserController {
     @Autowired
     private CreateUserUseCase createUserUseCase;
 
+    @Autowired
+    private GetAllEditorsUseCase getAllEditorsUseCase;
+
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody @Valid RegisterUserDTO dto) {
+    public ResponseEntity<ResponseUserDTO> createUser(@RequestBody @Valid RegisterUserDTO dto) {
         ResponseUserDTO response = createUserUseCase.execute(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/editors")
+    public ResponseEntity<List<ResponseUserDTO>> getEditors() {
+        List<ResponseUserDTO> editors = getAllEditorsUseCase.execute();
+        return ResponseEntity.status(HttpStatus.OK).body(editors);
     }
 }
