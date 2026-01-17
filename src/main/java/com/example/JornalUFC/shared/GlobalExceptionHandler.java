681c9f30330823589ab2515c01d821d8c56f2cf8
kauan <kauancavalcante8p@gmail.com>
2026-01-16 22:21:59 -0300

    add: HttpMessageNotReadableException

--- src/main/java/com/example/JornalUFC/shared/GlobalExceptionHandler.java
+++ src/main/java/com/example/JornalUFC/shared/GlobalExceptionHandler.java
@@ -1,18 +1,60 @@
 package com.example.JornalUFC.shared;
 
 import com.example.JornalUFC.shared.custonExceptions.ResourceNotFoundException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex
+    ) {
+
+        String field = "request";
+        String message = "JSON inválido";
+
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException invalidFormatException) {
+
+            Class<?> targetType = invalidFormatException.getTargetType();
+
+            if (targetType.isEnum()) {
+                field = invalidFormatException.getPath()
+                        .stream()
+                        .map(ref -> ref.getFieldName())
+                        .findFirst()
+                        .orElse("enum");
+
+                String allowedValues = Arrays.toString(targetType.getEnumConstants());
+
+                message = "Valor inválido para o campo '" + field +
+                        "'. Valores aceitos: " + allowedValues;
+            }
+        }
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                field,
+                message,
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorResponseDTO>> handleValidationErrors(
