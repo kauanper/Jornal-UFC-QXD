6234d5d7949b5e5b8455112177bde9b2a4d02179
kauan <kauancavalcante8p@gmail.com>
2026-01-16 21:50:28 -0300

    add: ordem no enum de category

--- src/main/java/com/example/JornalUFC/modules/news/Category.java
+++ src/main/java/com/example/JornalUFC/modules/news/Category.java
@@ -1,12 +1,12 @@
 package com.example.JornalUFC.modules.news;
 
 public enum Category {
+    DESTAQUE("destaque"),
     PESQUISA("pesquisa"),
     EXTENSAO("extensao"),
     ENSINO("ensino"),
     EVENTOS("eventos"),
-    COMUNIDADE("comunidade"),
-    DESTAQUE("destaque");
+    COMUNIDADE("comunidade");
 
     private String value;
 
