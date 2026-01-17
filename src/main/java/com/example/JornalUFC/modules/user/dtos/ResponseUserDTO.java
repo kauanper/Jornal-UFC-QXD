57c54583ced941e357d3b053a4a09f5659522f16
kauan <kauancavalcante8p@gmail.com>
2026-01-17 03:23:13 -0300

    add: dto de ResponseUserDTO.java

---
+++ src/main/java/com/example/JornalUFC/modules/user/dtos/ResponseUserDTO.java
@@ -0,0 +1,13 @@
+package com.example.JornalUFC.modules.user.dtos;
+
+import com.example.JornalUFC.modules.user.UserRoles;
+
+import java.time.LocalDateTime;
+
+public record ResponseUserDTO(
+        Long id,
+        String nickname,
+        String username, // email
+        LocalDateTime createdAt,
+        UserRoles role
+) {}
