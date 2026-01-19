5ed9559e560c9c6809ab3869c2cee5ed81a84821
Miqueias Bento <121648472+MiqueiasBento@users.noreply.github.com>
2026-01-19 18:10:00 -0300

    config: adicionando imports de configuracao do cors

--- src/main/java/com/example/JornalUFC/config/security/SecurityConfig.java
+++ src/main/java/com/example/JornalUFC/config/security/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+import java.util.List;
 
 @EnableMethodSecurity()
 @Configuration
